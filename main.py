0fcb605c881ca3a0d2ffbd1392ad570d6cb9dacd
2V30 <el1zavetag1103@yandex.ru>
2025-02-23 12:44:45 +0300

    start window + moving blocks

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import sys
 import pygame
 import random
 
-#загрузка изображения для спрайтов
+pygame.init()
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+FCOLOR = WHITE
+
+
+# загрузка изображения для спрайтов
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
@@ -12,31 +22,64 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
-#спрайт одного блока
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def first_screen():
+    intro_text = ["Press any button to continue"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (800, 600))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 500
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('green'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 270
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return True
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+# спрайт одного блока
 class Block(pygame.sprite.Sprite):
-    #задаётся прямоугольником
+    # задаётся прямоугольником
     image = pygame.Surface([100, 20])
-    image.fill((255, 255, 255))
+    image.fill(BLACK)
 
     def __init__(self, group, clock):
         super().__init__(group)
         self.image = Block.image
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0, 350) #рандомное значения по горизонтали
-        self.rect.y = -100 #изначально блок находится в -100 координате по у
+        self.rect.x = random.randint(0, WIDTH - 50)  # рандомное значения по горизонтали
+        self.rect.y = -100  # изначально блок находится в -100 координате по у
         self.clock = clock
         all_sprites.draw(screen)
 
-    #передвижение блока на 150 пикс. ниже
+    # передвижение блока на 100 пикс. ниже
     def update(self):
-        screen.fill('blue')
-        if self.rect.y >= 800:
-            del self #когда доходит до края уничтожаем блок
+        screen.fill(FCOLOR)
+        if self.rect.y >= HEIGHT:
+            del self  # когда доходит до края уничтожаем блок
             return 0
-        self.rect.y += 150
+        self.rect.y += 100
         return self
 
-#совокупность всех блоков
+
+# совокупность всех блоков
 class Display(pygame.sprite.Sprite):
 
     def __init__(self, group, clock):
@@ -50,39 +93,41 @@ class Display(pygame.sprite.Sprite):
     def update(self):
         # добавляем новый юлок при передвижении персонажа(чтобы их бфло бесконечное количество)
         Block(all_sprites, self.clock)
-        screen.fill('blue')
-        all_sprites.update() #обновляем пложение всех блоков
+        screen.fill(FCOLOR)
+        all_sprites.update()  # обновляем пложение всех блоков
         clock.tick(10)
         return self
 
 
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 400, 800
+    size = WIDTH, HEIGHT
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Doodle prygaet')
-    screen.fill('blue')
+    screen.fill(FCOLOR)
     pygame.display.update()
     clock = pygame.time.Clock()
-    #группа спрайтов для блоков
-    all_sprites = pygame.sprite.Group()
-    #спрайт фона игры
-    game = Display(all_sprites, clock)
-    all_sprites.draw(screen)
-    pygame.display.flip()
-    running = True
-    while running:
-        pressed = pygame.key.get_pressed()
+    if first_screen():
+        screen.fill(FCOLOR)
+        pygame.display.update()
+        # группа спрайтов для блоков
+        all_sprites = pygame.sprite.Group()
+        # спрайт фона игры
+        game = Display(all_sprites, clock)
         all_sprites.draw(screen)
         pygame.display.flip()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            #если нажали правую или левую стрелку меняем фон
-            if event.type == pygame.KEYDOWN and (event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT):
+        running = True
+        while running:
+            pressed = pygame.key.get_pressed()
+            all_sprites.draw(screen)
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                # если нажали правую или левую стрелку меняем фон
+                if event.type == pygame.KEYDOWN and (event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT):
+                    game.update()
+            # если правая или левая кнопка зажаты меняем фон
+            if pressed[pygame.K_RIGHT] or pressed[pygame.K_LEFT]:
                 game.update()
-        #если правая или левая кнопка зажаты меняем фон
-        if pressed[pygame.K_RIGHT] or pressed[pygame.K_LEFT]:
-            game.update()
-    pygame.quit()
-
+        pygame.quit()
